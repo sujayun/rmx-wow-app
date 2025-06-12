45581db3e984f00adc8c37711c09ec79a6edc731
Sujay U N <sujayun@yahoo.com>
2025-06-12 19:31:07 +0530

    Added netlify-deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 export default defineConfig({
   plugins: [
@@ -12,5 +13,6 @@ export default defineConfig({
         v3_lazyRouteDiscovery: true,
       },
     }),
+    netlifyPlugin()
   ],
 });
